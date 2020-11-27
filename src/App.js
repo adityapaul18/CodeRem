bf0974173eaba7c5e8c6e00ebeaf3ddfd4dd41be
Aditya Paul <62583380+adityapaul18@users.noreply.github.com>
2020-11-27 18:01:54 +0530

    src filed addedd

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { useState ,useEffect} from 'react';
+import React from 'react';
+import './App.css';
+import {IconButton} from "@material-ui/core";
+import Ongoingcards from './Ongoingcards.js'
+import Upcomingcards from './Upcomingcards.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {  Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+// import 'react-tabs/style/react-tabs.css'; 
+import Navbar from './Navbar'
+import About from './About'
+function App() {
+  const [data, setData] = useState(null);
+ 
+    useEffect(() => {
+ 
+    const fetchData = async() => {
+        fetch(
+            `https://contesttrackerapi.herokuapp.com/`
+            )
+        .then(response => response.json())
+        .then(result => {
+          setData(result)
+         })
+         console.log(data)
+     }
+     fetchData();
+     
+    },[])
+
+    console.log(data?.result)
+
+
+  return (
+    <div className="App">
+        <div className="AppHeader">
+          <Router>
+               <Navbar/>
+            <Switch>
+              <Route path='/' />
+            </Switch>
+          </Router>
+          <h1 className="header">Code-Rem</h1>
+      </div>
+      <div className="AppBody">
+          <Tabs className="tabs" defaultIndex={0} onSelect={index => console.log(index)}>
+           <center>
+           <TabList className="tablist">
+            <Tab className="tab">
+                   Ongoing   
+            </Tab>
+            <Tab className="tab">
+                   Upcoming
+            </Tab>
+            </TabList>
+           </center>
+            <TabPanel>
+             <center>
+             <div className="ongoing_cont">
+              {data?.result.ongoing.map(item => (
+                <Ongoingcards data={item}/>
+                ))
+              }
+          </div>
+             </center>
+            </TabPanel>
+            <TabPanel>
+            <div className="upcoming_cont">
+              <center>
+              {data?.result.upcoming.map(item => (
+                <Upcomingcards data={item}/>
+                ))
+              }
+              </center>
+          </div>
+            </TabPanel>
+          </Tabs>
+
+         Updated : {data?.result.timestamp}        
+         
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,49 @@
+import React,{ useState } from 'react'
+import {Link} from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { SidebarData } from './SidebarData';
+import { IconContext } from 'react-icons'
+import './Navbar.css';
+
+function Navbar() { 
+
+    const [ sidebar, setSidebar ] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar)
+
+    return (
+       <div className="mainbody">
+       <IconContext.Provider value={{color: 'white'}}>
+       <div className="navbar">
+            <Link to="#" className='menu-bars'>
+                <FaIcons.FaBars onClick={showSidebar} />
+            </Link>
+        </div>
+        <nav className = {sidebar ? 'nav-menu active' : 'nav-menu'} >
+            <ul className='nav-menu-items' onClick={showSidebar}>
+                <li className="navbar-toggle">
+                   <Link to="#" className='menu-bar'>
+                       <AiIcons.AiOutlineClose />
+                   </Link>
+                </li>
+                <div className="list">
+                {SidebarData.map((item,index) => {
+                    return(
+                        <li key={index} className={item.cName}>
+                            <a href={item.path} >
+                                {item.icon}
+                                <span>{item.title}</span>
+                            </a>
+                        </li>
+                    )
+                } )}
+                </div>
+            </ul>
+        </nav>
+       
+       </IconContext.Provider >
+       </div>
+    )
+}
+
+export default Navbar
