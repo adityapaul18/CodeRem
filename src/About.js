78c42f0694c25e7e9a699625eaea5cc27c7773a4
Aditya Paul <62583380+adityapaul18@users.noreply.github.com>
2021-02-18 18:02:55 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './About.css'
+function About() {
+    return (
+        <div className="header1">
+            <h1 className="header1">CodeRem</h1>
+        </div>
+    )
+}
+
+export default About
